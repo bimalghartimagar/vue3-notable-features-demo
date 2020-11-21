075a0c0f2dae2291200216563a1c6aa9e99b13bc
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-11-22 01:42:17 +0545

    making items same for demo
    added method to empty products

--- src/composables/useProducts.js
+++ src/composables/useProducts.js
@@ -2,16 +2,19 @@ import { ref } from "vue";
 
 export default function userProducts() {
   const products = ref([
-    { name: 'Custard', price: 32 },
-    { name: 'Pumpkin', price: 21 },
-    { name: 'Salt', price: 5 },
-    { name: 'Cottage Cheese', price: 17 }
+    {name: 'Milk', price: 2},
+    {name: 'Carrot', price: 12},
+    {name: 'Sugar', price: 8},
+    {name: 'Cheese', price: 20}
   ]);
 
   const addProduct = (product) => products.value.push(product);
 
+  const emptyProducts = () => products.value.splice(0);
+
   return {
     products,
-    addProduct
+    addProduct,
+    emptyProducts
   }
 }
